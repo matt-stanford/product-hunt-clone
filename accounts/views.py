ad45e07651ee419e52a2852b64f5343ee2927213
matt-stanford <mjfstanford@gmail.com>
2020-03-12 10:51:35 +0000

    user authentication added

--- accounts/views.py
+++ accounts/views.py
@@ -17,8 +17,22 @@ def signup(request):
     else:
         return render(request, 'accounts/signup.html', {})
 
+
+
 def login(request):
-    return render(request, 'accounts/login.html', {})
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error':'The username or password provided is incorrect'})
+    else:
+        return render(request, 'accounts/login.html', {})
+
+
 
 def logout(request):
-    return render(request, 'accounts/signup.html', {})
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
